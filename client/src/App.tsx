32090faf9d825acfb9cfbd0ff01675182e19c8f4
Andrew Dryuk <andrew.dryukad@gmail.com>
2022-01-04 21:23:20 +0300

    Client: add main App layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,47 @@
-import React from 'react';
+import React from "react";
+import "./App.scss";
+import { Header } from "./components/Header";
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+import { CartContent } from "./components/CartContent";
+import { TabsNamesEnum } from "./types";
+import {useFetchAllData} from "./hooks/useFetchAllData";
+
+const tabProps = {
+  className: "tab-list__item",
+  selectedClassName: "tab-list__item-active",
+  tabIndex: "0",
+};
 
 function App() {
+  useFetchAllData()
+
   return (
-    <div>
-     Hello world!
+    <div className="card">
+      <Header />
+      <main>
+        <Tabs>
+          <nav>
+            <TabList className="tab-list">
+              <Tab {...tabProps}>Last hour</Tab>
+              <Tab {...tabProps}>Today</Tab>
+              <Tab {...tabProps}>Yesterday</Tab>
+              <Tab {...tabProps}>Last 3 day</Tab>
+            </TabList>
+          </nav>
+          <TabPanel className="">
+            <CartContent tabName={TabsNamesEnum.errors_last_hour} />
+          </TabPanel>
+          <TabPanel>
+            <CartContent tabName={TabsNamesEnum.today} />
+          </TabPanel>
+          <TabPanel>
+            <CartContent tabName={TabsNamesEnum.errors_yesterday} />
+          </TabPanel>
+          <TabPanel>
+            <CartContent tabName={TabsNamesEnum.errors_last_3_day} />
+          </TabPanel>
+        </Tabs>
+      </main>
     </div>
   );
 }
--- server/App.ts
+++ server/App.ts
@@ -1,11 +1,15 @@
-import express from 'express'
+import express, {json} from 'express'
 import router from "./routes";
+import cors from 'cors'
+
 import errorResponseHandler from "./error/errorResponseHandler";
 import pageNotFoundMiddleware from "./middleware/pageNotFoundMiddleware";
 
 const PORT = process.env.PORT || '8080'
 const app = express()
 
+app.use(json())
+app.use(cors())
 app.use('/api', router)
 // Handle wrong route 404
 app.get('*', pageNotFoundMiddleware)
