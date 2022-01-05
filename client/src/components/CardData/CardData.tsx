3faa630a85afd57819cf7c75b8cb9d420686e0b5
Andrew Dryuk <andrew.dryukad@gmail.com>
2022-01-06 00:25:24 +0300

    Client: Add CartData component

---
+++ client/src/components/CardData/CardData.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import CartDataItem from "./CartDataItem";
+
+import './CardData.scss'
+import useGetPropsForCardSection from "../../hooks/useGetPropsForCardSection";
+
+const CardData = () => {
+  const {SearchesProps, ClickProps, BookingProps} = useGetPropsForCardSection()
+
+  return (
+    <section className="card-data">
+      <CartDataItem {...SearchesProps}/>
+      <CartDataItem {...ClickProps}/>
+      <CartDataItem {...BookingProps}/>
+    </section>
+  );
+};
+
+export default CardData
---
+++ client/src/components/CardData/CartDataItem.tsx
@@ -0,0 +1,91 @@
+import React, {FC, ReactElement} from 'react';
+import cn from "classnames";
+
+export interface CardDataItemProps {
+  svgIcon: ReactElement,
+  mainTitle: string,
+  label?: string,
+  colorRed?: true
+  errorsYesterday: string | number | null,
+  errorsLastFriday: string | number | null,
+  detailsTitle: string[],
+  detailsDescription: string,
+  helpLinks: string[]
+}
+
+const CartDataItem: FC<CardDataItemProps> = (props) => {
+  const {
+    svgIcon,
+    label,
+    detailsDescription,
+    detailsTitle,
+    mainTitle,
+    errorsLastFriday,
+    errorsYesterday,
+    helpLinks,
+    colorRed
+  } = props
+
+  return (
+    <div className="card-data__item">
+      <div className="card-data__main card-data__container">
+        <div className="card-data__icon">
+          {svgIcon}
+          <div className={cn('card-data__indicator', {
+            'card-data__background-red': colorRed
+          })}/>
+        </div>
+        <div>
+          <div className="card-data__title">
+            <h2 className={cn('header text', {
+              'card-data__text-red': colorRed
+            })}>
+              {mainTitle}
+            </h2>
+            {label && (
+              <div className={cn('card-data__label', {
+                'card-data__background-red': colorRed
+              })}>
+                {label}
+              </div>
+            )}
+          </div>
+          <div className="card-data__subtitle">
+            <div className="card-data__info-by-date">
+              {errorsYesterday ?? 'Null'}
+              <span>Yesterday</span>
+            </div>
+            <div className="card-data__info-by-date text-gray">
+              {errorsLastFriday ?? 'Null'}
+              <span>Last Friday</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="card-data__details card-data__container">
+        <div className={cn('card-data__title_column', {
+          'card-data__text-red': colorRed
+        })}>
+          {detailsTitle.map(textItem => (
+            <div key={textItem} className="header">
+              {textItem}
+            </div>
+          ))}
+        </div>
+        <div className="card-data__description">
+          {detailsDescription}
+        </div>
+        <div className="card-data__help">
+          Help:
+          {helpLinks.map(textItem => (
+            <span key={textItem} className="card-data__link">
+              {textItem}
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CartDataItem;
