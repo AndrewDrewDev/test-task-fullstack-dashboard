5a72f21b1f605c1f64f3aa943f7e0fad20d7a866
Andrew Dryuk <andrew.dryukad@gmail.com>
2022-01-06 13:28:40 +0300

    Fix: add api url(hardcode)

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 export const api = axios.create({
   // hardcode url to api
   baseURL: process.env.NODE_ENV === 'development'
-    ? 'https://test-task-fullstack-dashboard.herokuapp.com/'
-    : 'http://localhost:8080/',
+    ? 'http://localhost:8080/'
+    : 'https://test-task-fullstack-dashboard.herokuapp.com/',
 })
